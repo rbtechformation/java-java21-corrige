49aada3a43cb500738f00da6b57c2da88e3d1fec
rbonnamy <rbonnamy@diginamic.fr>
2026-07-24 11:52:57 +0200

    Correction TP 3

--- src/test/java/java17/ex05/Function_05_Test.java
+++ src/test/java/java17/ex05/Function_05_Test.java
@@ -1,5 +1,6 @@
 package java17.ex05;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -7,6 +8,8 @@ import org.junit.Test;
 
 import java17.data.Data;
 import java17.data.Person;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Exercice 5 - java.util.function.Consumer
@@ -16,24 +19,26 @@ public class Function_05_Test {
     //tag::functions[]
     // TODO compléter la fonction
     // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+    Consumer<Person> changePasswordToSecret = p -> p.setPassword("secret");
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+    Consumer<Person> verifyAge = p -> assertTrue(p.getAge() > 4);
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> verifyPassword = p -> {assert p.getPassword().equals("secret");};
     //end::functions[]
 
 
     @Test
     public void test_consumer() throws Exception {
         List<Person> personList = Data.buildPersonList();
+        Comparator<Person> comp = Comparator.comparing(p->p.getAge());
 
         // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
         // personList.forEach...
+        personList.forEach(changePasswordToSecret);
 
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
