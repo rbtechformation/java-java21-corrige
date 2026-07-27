2de6445dbd176189f0ae75af7e73423ab3621c91
rbonnamy <rbonnamy@diginamic.fr>
2026-07-27 12:04:11 +0200

    Mise au point TP 6 avec java 25 et nouvelles librairies

--- src/test/java/java25/ex01/Optional_01_Test.java
+++ src/test/java/java25/ex01/Optional_01_Test.java
@@ -1,16 +1,16 @@
-package java21.ex01;
+package java25.ex01;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Test;
-
-import java21.data.Data;
-import java21.data.Person;
+import java25.data.Data;
+import java25.data.Person;
+import org.junit.jupiter.api.Test;
 
 /**
  * Exercice 02 - Filter, Map
@@ -28,23 +28,24 @@ public class Optional_01_Test {
 
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(p->p.getAge()==18).findFirst();
         assertThat(optPerson.isPresent(), is(true));
         
         // TODO afficher la personne en question si l'optional contient une personne
     }
 
-    @Test(expected=NotPresentException.class)
+    @Test
     public void test_optional_notPresent() throws Exception {
     	List<Person> persons = Data.buildPersonList(50);
 
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(p->p.getAge()==75).findFirst();
         assertThat(optPerson.isPresent(), is(false));
         
         // TODO si la personne n'existe pas, jeter une exception NotPresentException
         // TODO utiliser la méthode "orElseThrow"
+        assertThrows(NotPresentException.class, () -> persons.stream().filter(p->p.getAge()==75).findFirst().orElseThrow(()->new NotPresentException()));
 
     }
 }
